7165be25b33b2ded15be217e9f9492814e9157f8
Нина <n.sekret@2gis.ru>
2019-01-31 16:18:15 +0700

    final version layout

--- main.js
+++ main.js
@@ -1,14 +1,50 @@
-menu.onclick = function menuHandler() {
-	const headerItem = document.getElementsByClassName('header_list_item');
-	const isResponsive = headerItem[0].classList.contains('responsive');
-	
-	for (var i = 0; i < headerItem.length; i++) {
-		headerItem[i].classList.add('responsive');
+// const menuItems = document.getElementsByClassName('menu');
+
+// for (var i = 0; i < menuItems.length; i++) {
+// 	menuBtn = menuItems[i].getElementsByClassName('menu_btn')[0];
+
+// 	menuBtn.onclick = function() {
+// 		menuList = this.parentNode.getElementsByClassName('menu_list')[0];
+
+// 		const menuIsOpend = menuList.classList.contains('_is-opened');
+
+// 		if (menuIsOpend) {
+// 			menuList.classList.remove('_is-opened');
+// 		} else {
+// 			menuList.classList.add('_is-opened');
+// 		}
+// 	};
+// }
+
+
+document.addEventListener("click", clickHandler);
+
+function clickHandler(event) {
+	let target = event.target;
+
+	// if (target.parentNode && target.parentNode.className == 'menu_btn') {
+	// 	menuBtnHandler(target.parentNode);
+	// }
+
+	while (target.className != 'menu') {
+		if (target.className == 'menu_btn') {
+			menuBtnHandler(target);
+
+			return;
+		}
+		target = target.parentNode;
 	}
-
-	if (isResponsive) {
-			for (var i = 0; i < headerItem.length; i++) {
-				 headerItem[i].classList.remove('responsive');
-	}}
-
 }
+
+function menuBtnHandler(button) {
+	menuList = button.parentNode.getElementsByClassName('menu_list')[0];
+
+	const menuIsOpend = menuList.classList.contains('_is-opened');
+
+	if (menuIsOpend) {
+		menuList.classList.remove('_is-opened');
+	} else {
+		menuList.classList.add('_is-opened');
+	}
+};
+
